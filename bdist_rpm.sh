72f933cda4dc951db7ec6b5055793f253ef3df54
Andy Georges <itkovian@gmail.com>
2018-04-26 16:25:33 +0200

    Typo

--- bdist_rpm.sh
+++ bdist_rpm.sh
@@ -7,7 +7,7 @@ mkdir -p BUILD SOURCES SPECS RPMS BUILDROOT
 ./autogen.sh
 ./configure
 make dist
-cp lbn-nhc-*.tar.gz SOURCES
+cp lbnl-nhc-*.tar.gz SOURCES
 cp ${PACKAGE}.spec "SPECS"
 rpmbuild --define "_topdir $PWD" -ba SPECS/${PACKAGE}.spec
 rm -rf BUILD SOURCES BUILDROOT SPECS
